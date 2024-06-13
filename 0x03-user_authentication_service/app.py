ddcb16b31485cbf3566a4b83dc9b9e22684dbfad
thugginnn <paul.iyiade4@gmail.com>
2024-06-13 23:55:24 +0000

    user function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,3 +24,18 @@ def index() -> str:
     {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    Register new users
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{email}", "message": "user created"})
