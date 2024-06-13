3cedb2961d3adc63200dc9d04d77e086afacdd94
thugginnn <paul.iyiade4@gmail.com>
2024-06-13 23:52:22 +0000

    JSON payload

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    request,
+    jsonify,
+    abort,
+    redirect,
+    url_for
+)
+
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """
+    Return json respomse
+    {"message": "Bienvenue"}
+    """
+    return jsonify({"message": "Bienvenue"})
